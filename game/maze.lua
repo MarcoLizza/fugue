6a955b6f0a2f82fdf73729694584678aeafbcaf5
Marco Lizza <marco.lizza@gmail.com>
2016-02-17 23:52:26 +0100

    Adding maze (dead-ends) reduction.

--- game/maze.lua
+++ game/maze.lua
@@ -77,17 +77,15 @@ local DELTAY = {
 local OPPOSITE = {
   n = 's', s = 'n', e = 'w', w = 'e',
 }
-local VALUE = {
-  n = 1, s = 2, e = 4, w = 8,
-}
+
 
 local function walk(grid, width, height, x, y)
   local directions = array.shuffle({ 'n', 's', 'e', 'w' })
   for _, direction in ipairs(directions) do
     local nx, ny = x + DELTAX[direction], y + DELTAY[direction]
-    if nx >= 1 and ny >= 1 and ny <= height and nx <= width and grid[ny][nx] == 0 then
-      grid[y][x] = grid[y][x] + VALUE[direction]
-      grid[ny][nx] = grid[ny][nx] + VALUE[OPPOSITE[direction]]
+    if nx >= 1 and ny >= 1 and ny <= height and nx <= width and #grid[ny][nx] == 0 then
+      table.insert(grid[y][x], direction)
+      table.insert(grid[ny][nx], OPPOSITE[direction])
       return nx, ny
     end
   end
@@ -98,25 +96,25 @@ end
 local function hunt(grid, width, height)
   for y = 1, height do
     for x = 1, width do
-      if grid[y][x] == 0 then -- unvisited cell
+      if #grid[y][x] == 0 then -- unvisited cell
         local neighbours = {}
-        if y > 1 and grid[y - 1][x] ~= 0 then
-          neighbours[#neighbours + 1] = 'n'
+        if y > 1 and #grid[y - 1][x] > 0 then
+          table.insert(neighbours, 'n')
         end
-        if x > 1 and grid[y][x - 1] ~= 0 then
-          neighbours[#neighbours + 1] = 'w'
+        if x > 1 and #grid[y][x - 1] > 0 then
+          table.insert(neighbours, 'w')
         end
-        if x + 1 < width and grid[y][x + 1] ~= 0 then
-          neighbours[#neighbours + 1] = 'e'
+        if x + 1 < width and #grid[y][x + 1] > 0 then
+          table.insert(neighbours, 'e')
         end
-        if y + 1 < height and grid[y + 1][x] ~= 0 then
-          neighbours[#neighbours + 1] = 's'
+        if y + 1 < height and #grid[y + 1][x] > 0 then
+          table.insert(neighbours, 's')
         end
         if #neighbours > 0 then -- at least a valid neighbour
           local direction = neighbours[love.math.random(#neighbours)]
           local nx, ny = x + DELTAX[direction], y + DELTAY[direction]
-          grid[y][x] = grid[y][x] + VALUE[direction]
-          grid[ny][nx] = grid[ny][nx] + VALUE[OPPOSITE[direction]]
+          table.insert(grid[y][x], direction)
+          table.insert(grid[ny][nx], OPPOSITE[direction])
           return nx, ny
         end
       end
@@ -124,6 +122,26 @@ local function hunt(grid, width, height)
   end
 
   return nil, nil
+end
+
+local function reduce(grid, width, height, amount)
+  while true do
+--    local oy = love.math.random(height)
+    for y = 1, height do
+      for x = 1, width do
+        if #grid[y][x] == 1 then
+          local direction = grid[y][x][1]
+          local nx, ny = x + DELTAX[direction], y + DELTAY[direction]
+          grid[y][x] = {}
+          array.remove(grid[ny][nx], OPPOSITE[direction])
+          amount = amount - 1
+          if amount == 0 then
+            return
+          end
+        end
+      end
+    end
+  end
 end
 
 local function overlaps(a, b)
@@ -161,7 +179,7 @@ end
 function Maze:generate()
   local width, height = self.width / 2, self.height / 2
   local grid = array.create(width, height, function(x, y)
-      return 0
+      return {}
     end)
   
   local x, y = love.math.random(width), love.math.random(height)
@@ -175,40 +193,40 @@ function Maze:generate()
     end
   end
   
+  reduce(grid, width, height, 250)
+  
   for y = 1, height do
     local yy = y * 2
     for x = 1, width do
       local xx = x * 2
-      self.visibility[yy][xx] = true
-      local value = grid[y][x]
-      if value >= 8 then
-        self.visibility[yy][xx - 1] = true
-        value = value - 8
-      end
-      if value >= 4 then
-        self.visibility[yy][xx + 1] = true
-        value = value - 4
-      end
-      if value >= 2 then
-        self.visibility[yy + 1][xx] = true
-        value = value - 2
-      end
-      if value >= 1 then
-        self.visibility[yy - 1][xx] = true
-        value = value - 1
+      local directions = grid[y][x]
+      if #directions > 0 then
+        self.visibility[yy][xx] = true
+        if array.contains(directions, 'w') then
+          self.visibility[yy][xx - 1] = true
+        end
+        if array.contains(directions, 'e') then
+          self.visibility[yy][xx + 1] = true
+        end
+        if array.contains(directions, 's') then
+          self.visibility[yy + 1][xx] = true
+        end
+        if array.contains(directions, 'n') then
+          self.visibility[yy - 1][xx] = true
+        end
       end
     end
   end
 
-  local rooms = self:generate_rooms(10000, 1, 5, 3, 10)
+--  local rooms = self:generate_rooms(10000, 1, 5, 3, 10)
 
-  for _, room in ipairs(rooms) do
-    for y = room.top, room.bottom do
-      for x = room.left, room.right do
-        self.visibility[y][x] = true
-      end
-    end
-  end
+--  for _, room in ipairs(rooms) do
+--    for y = room.top, room.bottom do
+--      for x = room.left, room.right do
+--        self.visibility[y][x] = true
+--      end
+--    end
+--  end
 end
 
 function Maze:spawn_emitter(id, x, y, radius, energy, duration)
